84284aecfd1e56c4af8d03ff2607366d8e26ef08
asbyth <cyronize@gmail.com>
2020-01-19 12:22:08 -0500

    change exception

--- src/main/java/club/sk1er/mods/thanos/ThanosMod.java
+++ src/main/java/club/sk1er/mods/thanos/ThanosMod.java
@@ -33,20 +33,25 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import org.apache.commons.io.FileUtils;
 
-import java.awt.*;
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.*;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Mod(modid = ThanosMod.MODID, version = ThanosMod.VERSION)
 public class ThanosMod {
     public static final String MODID = "thanosmod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.2";
     public static ThanosMod instance;
     public HashMap<UUID, Integer> renderBlacklist = new HashMap<>();
     private List<UUID> realEntity = new ArrayList<>();
@@ -430,7 +435,7 @@ public class ThanosMod {
                 enabled = object.get("enabled").getAsBoolean();
             if (object.has("speed"))
                 speed = object.get("speed").getAsDouble();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
