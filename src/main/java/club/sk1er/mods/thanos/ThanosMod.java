714a66c52d684682255f00814576ca5acbef9b55
asbyth <cyronize@gmail.com>
2020-01-19 12:33:23 -0500

    change exception 1.10

--- src/main/java/club/sk1er/mods/thanos/ThanosMod.java
+++ src/main/java/club/sk1er/mods/thanos/ThanosMod.java
@@ -47,7 +47,7 @@ import java.util.concurrent.ThreadLocalRandom;
 @Mod(modid = ThanosMod.MODID, version = ThanosMod.VERSION, acceptedMinecraftVersions = "*")
 public class ThanosMod {
     public static final String MODID = "thanosmod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.2";
     public static ThanosMod instance;
     public HashMap<UUID, Integer> renderBlacklist = new HashMap<>();
     public int DISTANCE = 16;
@@ -202,6 +202,7 @@ public class ThanosMod {
                 dust(((EntityPlayer) entity));
         }
     }
+
     private float seed;
 
     public boolean dust(EntityPlayer player) {
@@ -413,7 +414,7 @@ public class ThanosMod {
                 enabled = object.get("enabled").getAsBoolean();
             if (object.has("speed"))
                 speed = object.get("speed").getAsDouble();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
